db6ace617b4a6447adc1aea4d84198dc1b800aa6
kidi970 <jacobjacob99@hotmail.com>
2026-09-09 14:51:58 +0200

    Resolve main branch conflict

--- Code/src/main.adb
+++ Code/src/main.adb
@@ -3,26 +3,12 @@ with MicroBit.DisplayRT;
 with MicroBit.DisplayRT.Symbols;
 with MicroBit.Buttons; use MicroBit.Buttons;
 use MicroBit;
+
 -- USN PROJECT TEMPLATE INTELLIGENT REAL-TIME SYSTEMS
--- Project name: [project name]
--- Project members: [name, name, .. ]
+-- Project name: ADAptive
+-- Project members: Jacob Hellumsand Utengen, Nicolay Olafsen
 
-
---This is a project template for the MicroBit v2, built against the embedded-nrf52833
---runtime (a Ravenscar/Jorvik-style profile) so we have a language-supported real-time
---OS for embedded targets. The runtime ships with the gnat_arm_elf toolchain that Alire
---installs, so there is nothing to copy by hand.
-
---Check out the many examples in the ADL for the MicroBit v2 to see:
---  1) How the various drivers can be used like the accelerometer,speaker  or wireless communication via radio
---  2) How ADA features like the Math library and Bounded_Vectors (Containers) can be used
---  3) How to structure your Ada tasks with a protected object to synchronize data or with entries to synchronize flow
---  4) How to perform a execution time analysis
---  5) How to integrate your Ada project with a Unity project over USB
-
--- Open a View > Cross Platforms > Serial Ports to see Put_Line output. Set the baud rate to 115.200
-procedure Main with Priority => 0 is
-
+procedure Main is
 begin
    Put_Line ("MY ADAptive BUTTON BUILD IS RUNNING");
 
@@ -44,3 +30,4 @@ begin
       end if;
    end loop;
 end Main;
+
